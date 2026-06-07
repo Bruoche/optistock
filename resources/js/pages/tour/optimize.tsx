76819964c020b562611473136f15c8ce95e3126c
Alice Mailhes <armand.mailhes@gmail.com>
2026-06-07 16:38:06 +0200

    Small display fix

--- resources/js/pages/tour/optimize.tsx
+++ resources/js/pages/tour/optimize.tsx
@@ -32,7 +32,7 @@ export default function TourOptimize() {
         <>
             <Head title="Optimize tour" />
 
-            <div className="flex h-[calc(100vh-4rem)] flex-col">
+            <div className="flex min-h-0 flex-1 flex-col">
                 <div className="min-h-0 flex-[2] overflow-hidden">
                     <TourMap stops={stops} onAddStop={addStop} addable={!isPending && !isDone}>
                         {isDone && <RouteLayer path={routePath} />}
